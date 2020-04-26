d508a754aa8d647bb69e13bc289372f5f918d40f
Hong Le <Hongle050@gmail.com>
2020-04-26 17:53:24 +0100

    L1

---
+++ BeginnersL1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BeginnersL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //If Statement
+
+            int a = 2;
+            if(a == 1)
+            {
+                Console.WriteLine("The answer was 1");
+            } else if(a == 2)
+            {
+                Console.Write("The answer was 2");
+            }
+
+            //While Statement
+
+            int b = 0;
+            while(b < 10)
+            {
+                Console.WriteLine("The value of b is" + b);
+
+                b++;
+            }
+
+            //For Statement
+
+            for(int c = 0; c < 10; c++)
+            {
+                Console.WriteLine("The value of a is" + a);
+            }
+
+            //Nested Loop
+
+            for(int d = 1; d <= 10; d++)
+            {
+                for(int e = 1; e < 10; e++)
+                {
+                    Console.WriteLine(d + " * " + e + "=" + (d * e));
+                }
+            }
+        }
+    }
+}
